c44f5c37bca70660a1b90e313cdf673b3d5d3a9b
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-02 11:03:49 -0800

    section 1 - introduction

---
+++ ElizaIsSilly/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ElizaIsSilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doctor.Intro());
+            var userInput = "";
+
+            while(!userInput.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                userInput = Console.ReadLine();
+                string response = Doctor.response(userInput);
+                Console.WriteLine(response);
+
+            }
+        }
+    }
+}
