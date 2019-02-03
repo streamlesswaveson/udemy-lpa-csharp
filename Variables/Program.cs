5e43db26f96d0a76cc41a4eb63021d2bc06a2bd8
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-02 16:50:50 -0800

    2-18 - variables, etc

---
+++ Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+                
+            int firstNumber = random.Next(2,10);
+            int secondNumber = random.Next(2,10);
+
+            // 'var' will infer the type
+            var subtraction = random.Next(2,10);
+
+            int answer;
+            string prompt = ". Press ENTER when ready";
+            Console.WriteLine("think of a number between 1-10" + prompt);
+            Console.ReadKey();
+            Console.WriteLine("Multiply your number by " + firstNumber + prompt);
+            Console.ReadKey();
+            Console.WriteLine("Now multiply your number by " + secondNumber + prompt);
+            Console.ReadKey();
+            Console.WriteLine("Divide the result by the number you originally thought of " + prompt);
+            Console.ReadKey();
+            Console.WriteLine("Now subtract " + subtraction + prompt);
+            Console.ReadKey();
+
+            answer = firstNumber * secondNumber - subtraction;
+            Console.WriteLine("the answer is " + answer);
+
+                }
+    }
+}
