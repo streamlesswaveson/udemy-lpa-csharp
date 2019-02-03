613d91f84e76040734a2a350afc8879047ea2ad9
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-02 18:07:55 -0800

    3-34 - string formatting

---
+++ StringFormatting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apples = "apples";
+            int appleQuantity = 8;
+            int applePrice = 100;
+            string oranges = "oranges";
+            int orangeQuantity = 12;
+            int orangePrice = 80;
+
+            string col1Heading = "Fruit";
+            string col2Heading = "Quantity";
+            string col3Heading = "Price";
+
+            const int  col1Space = -12;
+            const int col2Space = 8;
+            const int col3Space = 6;
+            Console.WriteLine($"{col1Heading, col1Space} {col2Heading, col2Space} {col3Heading, col3Space}");
+            Console.WriteLine($"{apples, col1Space} {appleQuantity, col2Space} {applePrice, col3Space}");
+            Console.WriteLine($"{oranges, col1Space} {orangeQuantity, col2Space} {orangePrice, col3Space}");
+                }
+    }
+}
---
+++ UserInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your name");
+            string yourName = Console.ReadLine();
+            Console.WriteLine($"your name is {yourName}");
+            Console.WriteLine("how old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{yourName} is {age} years old");
+        }
+    }
+}
